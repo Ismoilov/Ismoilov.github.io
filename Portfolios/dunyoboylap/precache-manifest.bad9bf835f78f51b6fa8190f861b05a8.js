c16de120393ddb398a5c95218b10d441cf395fa1
Ismoilov <di.murod.1999@gmail.com>
2020-04-24 21:08:32 +0500

    dunyo boylap is added

---
+++ Portfolios/dunyoboylap/precache-manifest.bad9bf835f78f51b6fa8190f861b05a8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519666f0a2a082c37d93d45b9dfcbfef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5754ca06c4b48e60d34e",
+    "url": "/static/css/2.956938e7.chunk.css"
+  },
+  {
+    "revision": "aff8da3f6dc318573dfc",
+    "url": "/static/css/main.02de19e4.chunk.css"
+  },
+  {
+    "revision": "5754ca06c4b48e60d34e",
+    "url": "/static/js/2.896a5b72.chunk.js"
+  },
+  {
+    "revision": "f385f8a8f595d86b386170d101237d26",
+    "url": "/static/js/2.896a5b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aff8da3f6dc318573dfc",
+    "url": "/static/js/main.d4da1c4b.chunk.js"
+  },
+  {
+    "revision": "18ef80f1aeea9fc8f166",
+    "url": "/static/js/runtime-main.b835e79a.js"
+  },
+  {
+    "revision": "bfc5c27b4333489a9b957b7798ccacf4",
+    "url": "/static/media/Logo.bfc5c27b.svg"
+  },
+  {
+    "revision": "7641109fc8e399b2268dde00fd0bb440",
+    "url": "/static/media/LogoLoader.7641109f.svg"
+  },
+  {
+    "revision": "8116545e23bc4dfff3a6e0921d15ca78",
+    "url": "/static/media/allNews.8116545e.jpg"
+  },
+  {
+    "revision": "d0945b02de9a2966308b2fd10ecaaf8d",
+    "url": "/static/media/gallery.d0945b02.png"
+  },
+  {
+    "revision": "d7b1e3b353d51beac66ae36f3d5dd96c",
+    "url": "/static/media/header-parallax.d7b1e3b3.jpg"
+  },
+  {
+    "revision": "7f1aaa01528be62bd6a1584af0d09d86",
+    "url": "/static/media/like.7f1aaa01.svg"
+  },
+  {
+    "revision": "2e3e9a6c0e91a3b507fe3f302b515f3c",
+    "url": "/static/media/manyRead.2e3e9a6c.png"
+  },
+  {
+    "revision": "a6d423f07564bbf161dcfdaef0a1aaac",
+    "url": "/static/media/photo.a6d423f0.svg"
+  },
+  {
+    "revision": "4805f1da116af99a228e0dfd77a07d50",
+    "url": "/static/media/programs1.4805f1da.png"
+  },
+  {
+    "revision": "80a2860d7c680d775318cd9bb5755d3f",
+    "url": "/static/media/right-arrow.80a2860d.svg"
+  },
+  {
+    "revision": "4e894712d1835f6967b17b034ebbb3ce",
+    "url": "/static/media/slider1.4e894712.png"
+  },
+  {
+    "revision": "6100bdaf3d4f58b64768d17feb61e77b",
+    "url": "/static/media/small-1.6100bdaf.png"
+  },
+  {
+    "revision": "67f962ff1eea536632a150a2cd781de1",
+    "url": "/static/media/small-2.67f962ff.png"
+  },
+  {
+    "revision": "5a502f70ad54cf06da45f24a1cd8cfb2",
+    "url": "/static/media/teleshows.5a502f70.jpg"
+  },
+  {
+    "revision": "99f7333c047c6a852617593d996e132f",
+    "url": "/static/media/travel_1.99f7333c.png"
+  },
+  {
+    "revision": "50fab4f753fa033886551abe20e608e3",
+    "url": "/static/media/travels-2.50fab4f7.jpg"
+  },
+  {
+    "revision": "b849f48b51730c3cd1d165a17219a857",
+    "url": "/static/media/travelsBg.b849f48b.jpg"
+  }
+]);
